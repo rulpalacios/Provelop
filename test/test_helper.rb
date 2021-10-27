ae9a59230276355ef1fa1cd24102948098ec58f2
Raul Palacios <palacios.raul02@gmail.com>
2021-10-27 07:48:30 -0500

    Adjust test_helper and add simple cov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'rails/test_help'
+require 'factory_bot_rails'
+require 'capybara'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
