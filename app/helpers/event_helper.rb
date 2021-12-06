b37f8ab96f7dc7d64baa4f4ee4036eb22a355e36
Raul Palacios <palacios.raul02@gmail.com>
2021-12-05 23:45:39 -0600

    Fix linter offenses

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module EventHelper
-
   def show_reservation(user, event)
     return true unless user.purchases.where(event_id: event.id).empty?
 
-    return false
+    false
   end
 end
