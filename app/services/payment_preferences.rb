e4d89c6619fbd367ff3d6be230437cabb92ed644
Raul Palacios <palacios.raul02@gmail.com>
2021-12-06 00:28:25 -0600

    Update domain for mercadopago callback

--- app/services/payment_preferences.rb
+++ app/services/payment_preferences.rb
@@ -29,7 +29,7 @@ class PaymentPreferences < ApplicationService
         }
       ],
       back_urls: {
-        success: "http://localhost:3000/mercadopago/success_payments/#{event.id}?user_id=#{current_user.id}",
+        success: "#{ENV['DOMAIN']}/mercadopago/success_payments/#{event.id}?user_id=#{current_user.id}",
         failure: 'http://localhost:3000/mercadopago/failure',
         pending: 'http://localhost:3000/mercadopago/pendings'
       },
