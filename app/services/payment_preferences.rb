33e68da1e249b7d7ccf3ca4d8c4c62a0e1a6d7fe
Raul Palacios <palacios.raul02@gmail.com>
2021-12-06 21:22:43 -0600

    Move from key tests and mercado pago sandbox to production mode

--- app/services/payment_preferences.rb
+++ app/services/payment_preferences.rb
@@ -30,8 +30,8 @@ class PaymentPreferences < ApplicationService
       ],
       back_urls: {
         success: "https://provelopapp.herokuapp.com/mercadopago/success_payments/#{event.id}?user_id=#{current_user.id}",
-        failure: 'http://localhost:3000/mercadopago/failure',
-        pending: 'http://localhost:3000/mercadopago/pendings'
+        # failure: 'http://localhost:3000/mercadopago/failure',
+        # pending: 'http://localhost:3000/mercadopago/pendings'
       },
       auto_return: 'approved'
     }
