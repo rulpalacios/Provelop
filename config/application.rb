8655d84ad5d9972d7c04dabc75568231fd9d0936
Raul Palacios <palacios.raul02@gmail.com>
2021-12-07 23:33:10 -0600

    Add new view for create events

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,11 @@ module Provelop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Mexico City'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :es
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = %i[es en]
   end
 end
