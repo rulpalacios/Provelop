a0bc9cbd0f05a36c8cee6c2d7bdc8193a7f839fa
Raul Palacios <palacios.raul02@gmail.com>
2022-02-08 11:50:01 -0600

    List draft events for admin user

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,8 +1,7 @@
 module Admin
   class EventsController < ApplicationController
     def index
-      # @events = Event.where.not(status: 'draft')
-      @events = Event.all
+      @events = Event.where.not(status: 'draft')
     end
   end
 end
