ed78959f1dbfc1706bdd6dce41dba2ab6b153d86
Raul Palacios <palacios.raul02@gmail.com>
2022-02-14 13:24:34 -0600

    add skip to omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
