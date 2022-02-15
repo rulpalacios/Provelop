b6a422cce95dff8963388e53bd91287f30fe7a58
Raul Palacios <palacios.raul02@gmail.com>
2022-02-15 01:14:43 -0600

    Share correct number of arguments to contact mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,9 @@
 class ContactsController < ApplicationController
   def create
     ContactMailer.contact_email(
-      email: params[:email],
-      name: params[:name],
-      message: params[:message]
+      params[:email],
+      params[:name],
+      params[:message]
     ).deliver_later
 
     redirect_to root_path, notice: 'Correo enviado, pronto nos pondremos en contacto.'
